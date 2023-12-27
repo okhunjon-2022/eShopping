16efa74de1e36e2a2a49bc57948d8529eede479d
okhunjon-2022 <okhunjonmakhmudov.1998@gmail.com>
2023-12-28 00:13:00 +0500

    lorem

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Announcement from "../../components/announcement/Announcement";
-import Slider from "../../components/slider/Slider";
+// import Slider from "../../components/slider/Slider";
 
 function Home() {
   return (
